87049c2ee8f484582b8f348c3fa9bae7e4c6dd72
ZAPTA Technologies <shahidafridi26360@gmail.com>
2025-07-01 19:16:22 +0500

    middleware changes

--- src/middleware.js
+++ src/middleware.js
@@ -3,24 +3,28 @@ import { NextResponse } from 'next/server';
 export function middleware(req) {
   const { pathname, origin } = req.nextUrl;
   const token = req.cookies.get('auth-token')?.value;
+  const protectedRoutes = ['/clinics-reporting', '/submission', '/review'];
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
 
-  if (token && ['/login'].includes(pathname)) {
+  if (token && pathname === '/login') {
     return NextResponse.redirect(`${origin}/clinics-reporting`);
   }
 
   // Redirect to login if not authenticated and trying to access protected pages
-  if (!token && !['/login'].includes(pathname)) {
+  if (!token && isProtectedRoute) {
     return NextResponse.redirect(`${origin}/login`);
   }
 
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: [
-    '/login',
-    '/clinics-reporting',
-    '/submission/:path*',
-    '/review/:path*'
-  ]
-};
+// export const config = {
+//   matcher: [
+//     '/login',
+//     '/clinics-reporting',
+//     '/submission/:path*',
+//     '/review/:path*'
+//   ]
+// };
