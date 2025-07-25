fedad751d4699cc68f96923c12488c3a835e0d3b
abdul-rehman90 <abdul.rehman908909@gmail.com>
2025-07-25 18:21:45 +0500

    review chages

--- src/components/review/eom/supplies.jsx
+++ src/components/review/eom/supplies.jsx
@@ -1,33 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 import { GenericTable } from '@/common/components/table/table';
+import { EODReportService } from '@/common/services/eod-report';
 import { useGlobalContext } from '@/common/context/global-context';
 import StepNavigation from '@/common/components/step-navigation/step-navigation';
 
 export default function Supplies({ onNext }) {
-  const { getCurrentStepData } = useGlobalContext();
+  const { reportData, getCurrentStepData } = useGlobalContext();
   const currentStepData = getCurrentStepData();
+  const [dataLoading, setDataLoading] = useState(false);
+  const [totalSupplies, setTotalSupplies] = useState([]);
   const [tableData, setTableData] = useState([
     {
       key: '1',
-      budget: 0,
       actual: '',
-      reason: '',
-      difference: '-'
+      reason: ''
     }
   ]);
+  const clinicId = reportData?.eom?.basic?.clinic;
+  const submission_month = dayjs(
+    reportData?.eom?.basic?.submission_month
+  ).format('YYYY-MM');
 
   const columns = [
     {
       title: '',
-      width: 80,
+      width: 100,
       key: 'summary',
       dataIndex: 'summary',
       render: () => (
-        <div className="text-[15px] text-gray-900 font-bold">Total</div>
+        <div className="text-[15px] text-gray-900 font-bold">
+          Total Supplies
+        </div>
       )
     },
     {
-      width: 100,
+      width: 50,
       key: 'actual',
       disabled: true,
       editable: true,
@@ -36,20 +44,7 @@ export default function Supplies({ onNext }) {
       dataIndex: 'actual'
     },
     {
-      width: 100,
-      key: 'budget',
-      dataIndex: 'budget',
-      title: 'Budget (Goal)',
-      render: (_, record) => record.budget
-    },
-    {
-      width: 100,
-      title: '+/-',
-      key: 'difference',
-      render: (_, record) => record.difference
-    },
-    {
-      width: 100,
+      width: 300,
       key: 'reason',
       disabled: true,
       editable: true,
@@ -59,27 +54,107 @@ export default function Supplies({ onNext }) {
     }
   ];
 
+  const totalSuppliesColumns = [
+    {
+      width: 50,
+      key: 'submission_date',
+      title: 'Submission Date',
+      dataIndex: 'submission_date'
+    },
+    {
+      width: 50,
+      title: 'Actual',
+      key: 'supplies_actual',
+      dataIndex: 'supplies_actual'
+    },
+    {
+      width: 150,
+      key: 'overage_reason',
+      title: 'Reason for Overage',
+      dataIndex: 'overage_reason'
+    },
+    {
+      key: '',
+      width: 50,
+      dataIndex: '',
+      title: 'Monthly Budget'
+    },
+    {
+      key: '',
+      width: 50,
+      dataIndex: '',
+      title: 'Variance'
+    }
+  ];
+
+  const footer = () => {
+    const totalActual = totalSupplies.reduce(
+      (sum, item) => sum + (Number(item.supplies_actual) || 0),
+      0
+    );
+    return (
+      <div className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr] p-2">
+        <div className="font-semibold">Total</div>
+        <div className="ml-6">{totalActual}</div>
+        <div></div>
+        <div className="text-center">0</div>
+        <div
+          className="ml-15"
+          style={{
+            color: totalActual - 0 >= 0 ? 'green' : 'red'
+          }}
+        >
+          {totalActual - 0}
+        </div>
+      </div>
+    );
+  };
+
   useEffect(() => {
     if (Object.entries(currentStepData).length > 0) {
       const transformedData = [
         {
           key: '1',
           reason: currentStepData.overage_reason || '',
-          actual: currentStepData.supplies_actual || '',
-          budget: currentStepData.budget_daily_supplies || 0,
-          difference:
-            currentStepData.supplies_actual -
-              currentStepData.budget_daily_supplies || '-'
+          actual: currentStepData.supplies_actual || ''
         }
       ];
       setTableData(transformedData);
     }
   }, [currentStepData]);
 
+  useEffect(() => {
+    const getAllSupplies = async () => {
+      try {
+        setDataLoading(true);
+        const { data } = await EODReportService.getAllSupplies(
+          clinicId,
+          submission_month
+        );
+        setTotalSupplies(
+          data.map((item) => ({
+            ...item,
+            key: item.id
+          }))
+        );
+      } catch (error) {
+      } finally {
+        setDataLoading(false);
+      }
+    };
+    clinicId && getAllSupplies();
+  }, [clinicId]);
+
   return (
     <React.Fragment>
-      <div className="px-6">
+      <div className="px-6 flex flex-col gap-14">
         <GenericTable columns={columns} dataSource={tableData} />
+        <GenericTable
+          footer={footer}
+          loading={dataLoading}
+          dataSource={totalSupplies}
+          columns={totalSuppliesColumns}
+        />
       </div>
       <StepNavigation onNext={onNext} />
     </React.Fragment>
